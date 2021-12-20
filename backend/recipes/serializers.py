c4ba70f3a710a78fc423dd00726d4291b24e8ca4
Ivan Volkov <narmo@yandex.ru>
2021-12-20 15:23:15 +0300

    import pep8 issue

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,11 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.db.models import F
-
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.serializers import ValidationError
-from users.serializers import CustomUserSerializer
 
+from users.serializers import CustomUserSerializer
 from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                      ShoppingList, Tag)
 
